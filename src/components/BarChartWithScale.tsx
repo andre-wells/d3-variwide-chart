233112148d70423e1fbe4a596cb7a8e3656e66dc
Andre Wells <awells@bscglobal.com>
2023-02-15 15:04:17 +0200

    Updated scaled bar chart

--- src/components/BarChartWithScale.tsx
+++ src/components/BarChartWithScale.tsx
@@ -3,8 +3,8 @@ import React, { useEffect } from 'react';
 import { clearSvg, createSvg } from '../utils/svg';
 
 // Width and height
-const w = 960;
-const h = 500;
+const width = 960;
+const height = 500;
 const CONTAINER_ID = 'svg-bar-chart-scale-container';
 const SVG_ID = 'svg-bar-chart-scale';
 
@@ -29,12 +29,30 @@ const BarChartWithScale = () => {
   }, []);
 
   const Populate = (data: unknown) => {
-    const svg = createSvg(`#${CONTAINER_ID}`, SVG_ID, w, h);
+    const svg = createSvg(`#${CONTAINER_ID}`, SVG_ID, width, height);
 
     // Assign d.Population to the number of d.info.Population
     data.forEach((d) => (d.Population = +d.info.Population));
 
     // Set the domain of our scales.
+    const xScale = d3.scaleBand().range([0, width]); // 'band' is essentially categories
+    const yScale = d3.scaleLinear().range([height, 0]);
+
+    // The x scale is a band scale so we set the domain to the name of states (36 states)
+    xScale.domain(data.map((d) => d.Name));
+    // The y scale is a linear scale so we set the minimum value to 0. And rather than setting the maximum value ourselves, we let D3 do that for us by using the d3.max() method.
+    yScale.domain([0, d3.max(data, (d) => d.Population)]);
+
+    // Lastly we need to add the rectangles so we can see our bar chart:
+    svg
+      .selectAll('rect')
+      .data(data)
+      .join('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => xScale(d.Name))
+      .attr('y', (d) => yScale(d.Population))
+      .attr('width', xScale.bandwidth())
+      .attr('height', (d) => height - yScale(d.Population));
   };
 
   return <div id={CONTAINER_ID}></div>;
