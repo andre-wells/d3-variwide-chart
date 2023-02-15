d1e6994a80b53c607619fb4bb30ad90ee0d896b1
Andre Wells <awells@bscglobal.com>
2023-02-15 15:47:12 +0200

    Completed basic bar chart with scale

--- src/components/BarChartWithScale.tsx
+++ src/components/BarChartWithScale.tsx
@@ -1,10 +1,12 @@
 import * as d3 from 'd3';
 import React, { useEffect } from 'react';
+import { type IMargin } from '../models/margin';
 import { clearSvg, createSvg } from '../utils/svg';
 
 // Width and height
 const width = 960;
 const height = 500;
+const margin: IMargin = { top: 20, right: 30, bottom: 55, left: 70 };
 const CONTAINER_ID = 'svg-bar-chart-scale-container';
 const SVG_ID = 'svg-bar-chart-scale';
 
@@ -31,19 +33,33 @@ const BarChartWithScale = () => {
   const Populate = (data: unknown) => {
     const svg = createSvg(`#${CONTAINER_ID}`, SVG_ID, width, height);
 
+    const svg2 = d3.select(`#${CONTAINER_ID}`).attr('viewBox', [0, 0, width, height]);
+
+    // defines the position and dimension, in user space, of an SVG viewport.
+    // Basically, the viewbox of the svg are our dimentions.
+    // svg.attr('viewBox', [0, 0, width, height]);
+
     // Assign d.Population to the number of d.info.Population
     data.forEach((d) => (d.Population = +d.info.Population));
 
     // Set the domain of our scales.
-    const xScale = d3.scaleBand().range([0, width]); // 'band' is essentially categories
-    const yScale = d3.scaleLinear().range([height, 0]);
+    // Adjust the range to sit within our margines. We need the margins to show the axis for labels etc.
+    const xScale = d3
+      .scaleBand()
+      .range([margin.left, width - margin.right])
+      .padding(0.1); // 'band' is essentially categories
+    const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
 
     // The x scale is a band scale so we set the domain to the name of states (36 states)
     xScale.domain(data.map((d) => d.Name));
     // The y scale is a linear scale so we set the minimum value to 0. And rather than setting the maximum value ourselves, we let D3 do that for us by using the d3.max() method.
     yScale.domain([0, d3.max(data, (d) => d.Population)]);
 
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
     // Lastly we need to add the rectangles so we can see our bar chart:
+    // HOWEVER: After adding margins, we need to shift things.
     svg
       .selectAll('rect')
       .data(data)
@@ -52,7 +68,21 @@ const BarChartWithScale = () => {
       .attr('x', (d) => xScale(d.Name))
       .attr('y', (d) => yScale(d.Population))
       .attr('width', xScale.bandwidth())
-      .attr('height', (d) => height - yScale(d.Population));
+      .attr('height', (d) => height - margin.bottom - yScale(d.Population));
+
+    // append x axis
+    svg
+      .append('g')
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(xAxis)
+      .selectAll('text') // everything from this point is optional
+      .style('text-anchor', 'end')
+      .attr('dx', '-.8em')
+      .attr('dy', '.15em')
+      .attr('transform', 'rotate(-65)');
+
+    // append the y axis
+    svg.append('g').attr('transform', `translate(${margin.left},0)`).call(yAxis);
   };
 
   return <div id={CONTAINER_ID}></div>;
